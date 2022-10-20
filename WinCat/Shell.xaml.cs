d78739e2a1143ab37a2e174fcdd2cf603c84271b
Phuc Hoang <chphuc05@gmail.com>
2022-10-19 23:22:15 -0700

    Add project files.

---
+++ WinCat/Shell.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using WinCat.ShellPage;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinCat
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Shell : Window
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void mainNav_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                mainContentFrame.Navigate(typeof(MainSettingPage));
+            }
+            else
+            {
+                try
+                {
+                    var selectedItem = (NavigationViewItem)args.SelectedItem;
+                    string selectedItemTag = ((string)selectedItem.Tag);
+                    string pageName = "WinCat.ShellPage." + selectedItemTag;
+                    Type pageType = Type.GetType(pageName);
+                    mainContentFrame.Navigate(pageType);
+                }
+                catch (System.ArgumentNullException)
+                {
+                    ContentDialog dialog = new()
+                    {
+                        // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
+                        XamlRoot = sender.XamlRoot,
+                        Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+                        Title = "Page not found",
+                        PrimaryButtonText = "Ok",
+                        DefaultButton = ContentDialogButton.Primary,
+                        Content = new PageNotFoundPage()
+                    };
+                    _ = await dialog.ShowAsync();
+
+                }
+
+            }
+        }
+    }
+}
---
+++ WinCat/ShellPage/CodeRunnerPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinCat.ShellPage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CodeRunnerPage : Page
+    {
+        public CodeRunnerPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void scriptPicker_Click(object sender, RoutedEventArgs e)
+        {
+            var filePicker = new FileOpenPicker();
+
+            //Get the Window's HWND
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(Window.GetWindow(this));
+
+            // Associate the HWND with the file picker
+            WinRT.Interop.InitializeWithWindow.Initialize(filePicker, hwnd);
+
+            // Use file picker like normal!
+            filePicker.FileTypeFilter.Add("*");
+            StorageFile file = await filePicker.PickSingleFileAsync();
+            scriptPath.Text = file.Path;
+        }
+    }
+}
